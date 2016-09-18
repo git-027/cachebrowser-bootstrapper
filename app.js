deea053126ba164239a2527e148fc7c2a953fe76
ha-D <hadi.zolfaghaari@gmail.com>
2016-09-18 17:04:29 -0400

    Update CORS headers

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.use(express.static(path.join(__dirname, 'web/public')));
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 })
 
